bd517aa4563487125f1655b9b1c3070d975771eb
David Rachwalik <david.rachwalik@outlook.com>
2022-01-04 05:15:13 -0600

    Initial commit

---
+++ HtmlTypeScriptPort/Pages/Scripts/app.ts
@@ -0,0 +1,44 @@
+﻿import * as _ from 'lodash';
+import * as bootstrap from 'bootstrap';
+
+
+let message: string = 'Hello, world!';
+console.log(message);
+
+
+interface Character {
+    name_first: string;
+    name_last: string;
+    [key: string]: any
+}
+
+const character1: Character = {
+    name_first: "Kimihito",
+    name_last: "Kurusu"
+}
+
+const character2: Character = {
+    name_first: "Suu",
+    name_last: ""
+}
+
+
+function welcomeCharacter(character: Character): string {
+    return `Hey, ${character.name_first} ${character.name_last}`;
+}
+let welcomeMessage = welcomeCharacter(character2);
+console.log(welcomeMessage);
+
+
+// -------- Enable BootStrap Tooltips (old method) --------
+// $(function() {
+//     // Enable Bootstrap tooltips everywhere // https://getbootstrap.com/docs/4.6/components/tooltips
+//     $('[data-bs-toggle="tooltip"]').tooltip();
+// });
+
+
+// -------- Enable BootStrap Tooltips --------
+let tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+let tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+});
---
+++ README.md
@@ -0,0 +1,17 @@
+# CorruptionOfChampions
+Archive of release 0.94 in ActionScript3 and conversion to C# and TypeScript.
+
+## Game Overview
+Corruption of Champions is an erotic text based and browser based flash game.
+
+---
+
+## Game Features
+The rules that govern and guide the player's actions, as well as the game's response to them
+
+* Corruption of Champions [Wiki](https://wiki.smutosaur.us/CoC/Main_Page "Erotic browser based flash text game")
+
+
+## Technology Stack (Reference Links)
+* Markdown [Cheat Sheet](https://www.markdownguide.org/cheat-sheet "A quick reference to the Markdown syntax")
+* TypeScript [Official](https://www.typescriptlang.org/docs "TypeScript Official Documentation"), [VS Code](https://code.visualstudio.com/docs/languages/typescript "TypeScript in Visual Studio Code")
