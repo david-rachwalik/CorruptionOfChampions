b6813a97597e3ea6dd8660b247770d02b1f1edb5
David Rachwalik <david.rachwalik@outlook.com>
2022-01-26 22:40:03 -0600

    formatting update

--- HtmlTypeScriptPort/.prettierrc.cjs
+++ HtmlTypeScriptPort/.prettierrc.cjs
@@ -1,13 +1,13 @@
-// https://prettier.io/docs/en/configuration.html
-// https://prettier.io/docs/en/options.html
-// https://github.com/prettier/prettier-vscode#default-formatter
-// https://prettier.io/docs/en/configuration.html#sharing-configurations
 module.exports = {
-    endOfLine: "auto", // required for ESLint
-    semi: true,
-    trailingComma: "all",
-    singleQuote: false,
-    printWidth: 160,
-    tabWidth: 4,
-    bracketSameLine: true
+  // https://prettier.io/docs/en/configuration.html
+  // https://prettier.io/docs/en/options.html
+  // https://github.com/prettier/prettier-vscode#default-formatter
+  // https://prettier.io/docs/en/configuration.html#sharing-configurations
+  endOfLine: 'auto', // required for ESLint
+  semi: true,
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 160,
+  tabWidth: 2,
+  bracketSameLine: true
 };
--- HtmlTypeScriptPort/webpack.common.js
+++ HtmlTypeScriptPort/webpack.common.js
@@ -1,9 +1,9 @@
-import { fileURLToPath } from "url";
-import path from "path";
-import HtmlWebpackPlugin from "html-webpack-plugin";
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
-import postcssPresetEnv from "postcss-preset-env";
-import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
+import { fileURLToPath } from 'url';
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import postcssPresetEnv from 'postcss-preset-env';
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -11,63 +11,70 @@ const __dirname = path.dirname(__filename);
 // https://webpack.js.org/configuration/#options
 export const context = __dirname;
 export const entry = {
-    main: "./src/index.tsx",
-    // app: "./Scripts/corruption-of-champions/main.ts"
+  main: './src/index.tsx',
+  // app: "./Scripts/corruption-of-champions/main.ts"
 };
 export const output = {
-    path: path.resolve(__dirname, "wwwroot/dist"),
-    filename: "[name].bundle.js",
-    assetModuleFilename: "images/[hash]/[ext][query]",
-    clean: true,
+  path: path.resolve(__dirname, 'wwwroot/dist'),
+  publicPath: '/dist/',
+  filename: '[name].bundle.js',
+  assetModuleFilename: 'images/[hash]/[ext][query]',
+  clean: true,
 };
 export const resolve = {
-    extensions: [".tsx", ".ts", ".jsx", ".js"],
+  extensions: ['.tsx', '.ts', '.jsx', '.js'],
 };
 export const plugins = [
-    new MiniCssExtractPlugin(),
-    new HtmlWebpackPlugin({
-        template: "./src/index.html",
-    }),
-    // https://github.com/TypeStrong/fork-ts-checker-webpack-plugin/blob/main/examples/babel-loader/webpack.config.js
-    new ForkTsCheckerWebpackPlugin({
-        typescript: {
-            diagnosticOptions: {
-                semantic: true,
-                syntactic: true,
-            },
-            mode: "write-references",
-        },
-        eslint: {
-            files: "./src/**/*.{ts,tsx,js,jsx}",
-        },
-    }),
+  new MiniCssExtractPlugin(),
+  new HtmlWebpackPlugin({
+    template: './src/index.html',
+  }),
+  // https://github.com/TypeStrong/fork-ts-checker-webpack-plugin/blob/main/examples/babel-loader/webpack.config.js
+  new ForkTsCheckerWebpackPlugin({
+    typescript: {
+      diagnosticOptions: {
+        semantic: true,
+        syntactic: true,
+      },
+      mode: 'write-references',
+    },
+    eslint: {
+      files: './src/**/*.{ts,tsx,js,jsx}',
+    },
+  }),
 ];
 export const module = {
-    rules: [
+  rules: [
+    // https://webpack.js.org/guides/asset-modules
+    {
+      test: /\.(png|jpe?g|gif|svg)$/i,
+      type: 'asset',
+    },
+    // Inject font data into the bundle instead of emitting separate files
+    {
+      test: /\.(woff2?|eot|ttf|otf)$/,
+      type: 'asset/inline',
+    },
+    {
+      test: /\.(s[ac]|c)ss$/i,
+      use: [
         {
-            test: /\.(png|jpe?g|gif|svg)$/i,
-            type: "asset",
+          loader: MiniCssExtractPlugin.loader,
+          options: {
+            publicPath: '',
+          },
         },
+        { loader: 'css-loader', options: { importLoaders: 1 } },
         {
-            test: /\.(s[ac]|c)ss$/i,
-            use: [
-                {
-                    loader: MiniCssExtractPlugin.loader,
-                    options: {
-                        publicPath: "",
-                    },
-                },
-                { loader: "css-loader", options: { importLoaders: 1 } },
-                {
-                    loader: "postcss-loader",
-                    options: {
-                        postcssOptions: {
-                            plugins: [postcssPresetEnv()],
-                        },
-                    },
-                },
-                "sass-loader",
-            ],
+          loader: 'postcss-loader',
+          options: {
+            postcssOptions: {
+              plugins: [postcssPresetEnv()],
+            },
+          },
         },
-    ],
+        'sass-loader',
+      ],
+    },
+  ],
 };
