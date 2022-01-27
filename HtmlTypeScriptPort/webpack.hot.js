8a0942a46fa0d7b9487e5dc489f4ac9bfc18dfd2
David Rachwalik <david.rachwalik@outlook.com>
2022-01-27 09:47:40 -0600

    corrected HMR devServer path

--- HtmlTypeScriptPort/webpack.hot.js
+++ HtmlTypeScriptPort/webpack.hot.js
@@ -4,7 +4,7 @@ import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 
 export default merge(dev, {
   devServer: {
-    static: './dist',
+    static: './wwwroot',
     open: true, // https://webpack.js.org/configuration/dev-server/#devserveropen
     hot: true, // https://webpack.js.org/configuration/dev-server/#devserverhot
     // watch mode enabled by default in 'webpack-dev-server' (https://webpack.js.org/configuration/watch)
